5ca01bf8c946b84128e6a9e5c72e29ce7a208456
Lucas Lima <id.lucas@outlook.com.br>
2022-05-31 13:25:00 -0300

    Component Weather Data

--- src/components/WeatherData/index.tsx
+++ src/components/WeatherData/index.tsx
@@ -1,9 +1,15 @@
 import * as S from "./styles";
 
+import Ensolarado from '../../assets/icons/ensolarado.png';
+
 export default function WeatherData() {
   return (
     <S.Container>
-      Hello World
+      <img src={Ensolarado} alt="" />
+      <div>
+        <span>19º</span>
+        <p>location</p>
+      </div>
     </S.Container>
   );
 }
--- src/components/WeatherData/styles.ts
+++ src/components/WeatherData/styles.ts
@@ -1,5 +1,24 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
-  
+  background-color: ${({theme}) => theme.card};
+  border-radius: 25px;
+  padding: 25px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+
+  img {
+    width: 150px;
+  }
+
+  div {
+    span {
+      font-size: 60px;
+    }
+    > p {
+      margin: 10px 0 0 40px;
+    }
+  }
 `;
