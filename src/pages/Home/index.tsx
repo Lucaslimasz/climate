9587c06237cb1f2eb4ab969c988dd845b4444b93
Lucas Lima <id.lucas@outlook.com.br>
2022-05-31 08:50:24 -0300

    Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,13 @@
+import Header from '../../components/Header';
+import WeatherData from '../../components/WeatherData';
+
 import * as S from './styles';
 
 export default function Home(){
   return (
-    <h1>Hello World</h1>
+    <S.Container>
+      <Header />
+      <WeatherData />
+    </S.Container>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,5 @@
-import styles from 'styled-components';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 1.8rem 1.4rem;
+`;
