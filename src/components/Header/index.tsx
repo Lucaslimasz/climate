a2f49406c7a4506a84a2e0295049581f81493d70
Lucas Lima <id.lucas@outlook.com.br>
2022-05-31 08:50:02 -0300

    Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import * as S from "./styles";
+
+export default function Header() {
+  return (
+    <S.Container>
+      <h2>João Pessoa</h2>
+      <img src="" alt="theme" />
+    </S.Container>
+  );
+}
